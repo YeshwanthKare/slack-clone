f3d9d714526ee1ae1ebafb4c6b21218affb47ccb
YeshwanthKare <yesh.kare@gmail.com>
2022-09-25 17:01:06 +0530

    ADD: Sidebar component to the slack clone

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   // BEM naming convention
   return (
     <div className="App">
-      {/* {Header} */}
       <Header />
-      {/* Sidebar */}
+      <div className="app__body">
+        <Sidebar />
+      </div>
       {/* React-Router -> Chat Screen */}
     </div>
   );
---
+++ src/components/Sidebar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Sidebar.css";
+import {
+  InsertComment,
+  Inbox,
+  Drafts,
+  BookmarkBorder,
+  PeopleAlt,
+  Apps,
+  FileCopy,
+  ExpandLess,
+  ExpandMore,
+  Create,
+  FiberManualRecord,
+  Add,
+} from "@mui/icons-material";
+
+import SidebarOption from "./SidebarOption";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2>Clever Programmer</h2>
+          <h3>
+            <FiberManualRecord />
+            Yeshwanth Kare
+          </h3>
+        </div>
+        <Create />
+      </div>
+      <SidebarOption Icon={InsertComment} title="Threads" />
+      <SidebarOption Icon={Inbox} title="Mentions & reactions" />
+      <SidebarOption Icon={Drafts} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorder} title="Channel browser" />
+      <SidebarOption Icon={PeopleAlt} title="People & user groups" />
+      <SidebarOption Icon={Apps} title="Apps" />
+      <SidebarOption Icon={FileCopy} title="File browser" />
+      <SidebarOption Icon={ExpandLess} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMore} title="Channels" />
+      <hr />
+      <SidebarOption Icon={Add} addChannelOption title="Add Channel" />
+      {/* Connect to dB and list all the channels */}
+      {/* <SidebarOption ... /> */}
+    </div>
+  );
+};
+
+export default Sidebar;
